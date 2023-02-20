f2bc6c47b4d2782a375efcbe650cb1b0cdfa7834
T-erry <terrywachuka10@gmail.com>
2023-02-20 13:35:28 +0300

    refactored run file

--- bin/run.rb
+++ bin/run.rb
@@ -1,27 +1,27 @@
 require_relative  '../config/enviroment.rb'
 
 # create some authors
-author1 = Author.new("Stephen King")
-author2 = Author.new("J.K. Rowling")
-author3 = Author.new("Ernest Hemingway")
+author1 = Author.new("Jane Smith")
+author2 = Author.new("John Doe")
+author3 = Author.new("Austin Foo")
 
 # create some magazines
-magazine1 = Magazine.new("Time", "News")
-magazine2 = Magazine.new("Vogue", "Fashion")
-magazine3 = Magazine.new("National Geographic", "Science")
+magazine1 = Magazine.new("Vogue", "Fashion")
+magazine2 = Magazine.new("Tesla", "Technology")
+magazine3 = Magazine.new("CNN", "News")
 
 # add articles to authors and magazines
-author1.add_article(magazine1, "The Gunslinger")
-author1.add_article(magazine1, "The Shining")
-author2.add_article(magazine2, "Harry Potter and the Philosopher's Stone")
-author2.add_article(magazine2, "Harry Potter and the Chamber of Secrets")
-author3.add_article(magazine3, "The Old Man and the Sea")
-author3.add_article(magazine3, "A Farewell to Arms")
+author1.add_article(magazine1, "10 Ways to Rock Your Look")
+author1.add_article(magazine1,  "The Future of Sustainable Fashion")
+author2.add_article(magazine2,  "The Rise of AI in Fashion")
+author2.add_article(magazine2, "How to Code Your Own Chatbot")
+author3.add_article(magazine3, "Breaking News: COVID Vaccines for Kids Approved")
+author3.add_article(magazine3, "China provinces top list of regions at risk from climate change")
 
 # retrieve information using instance and class methods
 puts "Author 1's name is #{author1.name}"
 puts "Magazine 2's category is #{magazine2.category}"
-puts "The first magazine with the name 'Time' is #{Magazine.find_by_name("Time").name}"
+puts "The first magazine with the name 'Vogue' is #{Magazine.find_by_name("Vogue").name}"
 puts "Author 1's contributing topic areas are #{author1.topic_areas}"
 puts "The titles of all articles for magazine 1 are: #{magazine1.article_titles}"
 puts "Authors who have written more than 2 articles for magazine 2 are: #{magazine2.contributing_authors.map(&:name)}"
